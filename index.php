1d1ae2e4ab366b141206449ccbc369ec188e9ba5
ATHIRA SIVADASAN <75289141+athirasivadasan1999@users.noreply.github.com>
2022-03-05 19:46:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Pharmacy Management System</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom Fonts -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+<style>
+  body
+{
+	margin: 0;
+	padding: 0;
+	background: url(Pharmacy-new-670x500.jpg);
+	background-size: cover;
+	font-family: sans-serif;
+}
+h1
+{
+font-family: serif;
+padding: 15px 32px;
+font-size: 75px;
+margin-right: 55%;
+color: #cc0000 ;
+}
+h4
+{
+ font-size: 25px;
+ margin-left: 5%;
+}
+
+img
+{
+  height: 120px;
+  width: 120px;
+  margin-bottom: 0px;
+  margin-left: 850px;
+}
+a
+{
+  color: #002db3;
+  font-size: 25px;
+}
+a:hover {
+  color: green;
+}
+
+.horizontal-menu ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+.horizontal-menu li {
+    display: inline;
+}
+.horizontal-menu a {
+    display: block;
+    padding: 8px;
+    background-color: #0000ff;
+}
+.horizontal-menu .active {
+	background-color: red;
+    color: white;
+}
+.horizontal-menu ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #0018c3;
+}
+.horizontal-menu li {
+    float: left;
+}
+.horizontal-menu li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 5px 110px;
+    text-decoration: none;
+}
+.horizontal-menu li a:hover {
+    background-color: #9999ff;
+}
+.horizontal-menu li {
+    border-right: 1px solid #bbb;
+}
+ 
+.horizontal-menu li:last-child {
+    border-right: none;
+}
+
+</style>
+</head>
+
+<body id="page-top">
+<nav class="navbar navbar-default " role="navigation" >
+
+  <div class="container-fluid">
+    <div class="navbar-header">
+	  	<div class="horizontal-menu">
+        <ul>
+            <li><a href="#" class="active">HOME</a></li>
+            <li><a href="aboutus.php">ABOUT US</a></li>
+            <li><a href="contact.php">CONTACT</a></li>
+            <li><a href="help.php">HELP</a></li>
+            </ul>
+            
+        
+    </div>
+    </div>
+  </div>
+</nav>
+
+            
+
+
+  <!-- Header -->
+  <header>
+    <div>
+    <br>
+     <br><h1><b>MEDI-CARE</b></h1><br>
+     <h4>Pharmacy Management System<h4>
+     </div>
+    <div class="overlay"></div>
+  </header>
+
+ 
+
+
+
+  <a href="admin"><img src="images (3).jpg"/><b>Admin Login</b></a><br>
+  <br>
+  <br><a href="pharmacist"><img src="download.jpg"/><b>Pharmacist Login</b></a>
+  
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded js-scroll-trigger" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/stylish-portfolio.min.js"></script>
+
+</body>
+
+</html>
